27fbaf90180ef202e055e5151252b7924f2e2922
Michal Holasek <michal.holasek@apiary.io>
2016-11-28 18:53:03 +0100

    Add several test cases

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # http-zoo
+
+This repository contains a set of tests / specifications to test HTTP clients for various HTTP protocol edge cases.
+We are using [axios](https://github.com/mzabriskie/axios) HTTP client and [hamms](https://github.com/kevinburke/hamms) HTTP server so far.
+
+## Installation
+
+1. Install hamms by running ```pip install hamms``` in the command line (you must have Python 2.7 and pip installed on your machine).
+
+2. Run ```npm install``` to install dependencies
+
+## Usage
+
+1. Start hamms by running ```npm start``` in the command line:
+2. Open new command line tab and run ```npm test```
---
+++ test/badResponse.test.js
@@ -0,0 +1,47 @@
+const axios = require('axios');
+const expect = require('chai').expect;
+
+describe('Bad response', () => {
+
+  it('should handle no response gracefully', (done) => {
+    axios.get('http://localhost:5501', { timeout: 30000 })
+      .catch(err => {
+        expect(err.code).to.equal('ECONNABORTED');
+        done();
+      });
+  })
+  .timeout(35000);
+
+  it('should handle empty response gracefully (GET)', (done) => {
+    axios.get('http://localhost:5502', { timeout: 30000 })
+      .catch(err => {
+        expect(err.code).to.equal('ECONNRESET');
+        done();
+      });
+  });
+
+  it('should handle empty response gracefully (POST)', (done) => {
+    axios.post('http://localhost:5503', 'foo bar', { timeout: 30000 })
+      .catch(err => {
+        expect(err.code).to.equal('ECONNRESET');
+        done();
+      });
+  });
+
+  it('should handle malformed response (immediately upon connection) gracefully', (done) => {
+    axios.get('http://localhost:5504', 'foo bar', { timeout: 30000 })
+      .catch(err => {
+        expect(err.code).to.equal('HPE_INVALID_CONSTANT');
+        done();
+      });
+  });
+
+  it('should handle malformed response gracefully (POST)', (done) => {
+    axios.post('http://localhost:5505', 'foo bar', { timeout: 30000 })
+      .catch(err => {
+        expect(err.code).to.equal('HPE_INVALID_CONSTANT');
+        done();
+      });
+  });
+
+});
