dfca4cbd2f7ef0a50d10a6da30fed988703f8e4d
Michal Holasek <michal.holasek@apiary.io>
2016-11-29 13:34:24 +0100

    Add ESLint

--- test/badResponse.test.js
+++ test/badResponse.test.js
@@ -2,19 +2,17 @@ const axios = require('axios');
 const expect = require('chai').expect;
 
 describe('Bad response', () => {
-
   it('should handle no response gracefully', (done) => {
     axios.get('http://localhost:5501', { timeout: 30000 })
-      .catch(err => {
+      .catch((err) => {
         expect(err.code).to.equal('ECONNABORTED');
         done();
       });
-  })
-  .timeout(35000);
+  }).timeout(35000);
 
   it('should handle empty response gracefully (GET)', (done) => {
     axios.get('http://localhost:5502', { timeout: 30000 })
-      .catch(err => {
+      .catch((err) => {
         expect(err.code).to.equal('ECONNRESET');
         done();
       });
@@ -22,7 +20,7 @@ describe('Bad response', () => {
 
   it('should handle empty response gracefully (POST)', (done) => {
     axios.post('http://localhost:5503', 'foo bar', { timeout: 30000 })
-      .catch(err => {
+      .catch((err) => {
         expect(err.code).to.equal('ECONNRESET');
         done();
       });
@@ -30,7 +28,7 @@ describe('Bad response', () => {
 
   it('should handle malformed response (immediately upon connection) gracefully', (done) => {
     axios.get('http://localhost:5504', 'foo bar', { timeout: 30000 })
-      .catch(err => {
+      .catch((err) => {
         expect(err.code).to.equal('HPE_INVALID_CONSTANT');
         done();
       });
@@ -38,10 +36,9 @@ describe('Bad response', () => {
 
   it('should handle malformed response gracefully (POST)', (done) => {
     axios.post('http://localhost:5505', 'foo bar', { timeout: 30000 })
-      .catch(err => {
+      .catch((err) => {
         expect(err.code).to.equal('HPE_INVALID_CONSTANT');
         done();
       });
   });
-
 });
--- test/connectionFailure.test.js
+++ test/connectionFailure.test.js
@@ -2,15 +2,11 @@ const axios = require('axios');
 const expect = require('chai').expect;
 
 describe('Connection failure', () => {
-
   it('should handle connection timeout gracefully', (done) => {
     axios.get('http://www.google.com:81', { timeout: 30000 })
-      .then(res => console.log(res))
-      .catch(err => {
+      .catch((err) => {
         expect(err.code).to.equal('ECONNABORTED');
         done();
       });
-  })
-  .timeout(35000);
-
+  }).timeout(35000);
 });
