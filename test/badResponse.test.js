2b3c8c46482866265989d1dacbcac1a4a28b7f04
Michal Holasek <michal.holasek@apiary.io>
2016-11-29 13:40:18 +0100

    Add short description for each test case

--- test/badResponse.test.js
+++ test/badResponse.test.js
@@ -2,6 +2,7 @@ const axios = require('axios');
 const expect = require('chai').expect;
 
 describe('Bad response', () => {
+  // Server accepts traffic but never sends back data
   it('should handle no response gracefully', (done) => {
     axios.get('http://localhost:5501', { timeout: 30000 })
       .catch((err) => {
@@ -10,6 +11,7 @@ describe('Bad response', () => {
       });
   }).timeout(35000);
 
+  // Server sends back an empty string immediately upon connection
   it('should handle empty response gracefully (GET)', (done) => {
     axios.get('http://localhost:5502', { timeout: 30000 })
       .catch((err) => {
@@ -18,6 +20,7 @@ describe('Bad response', () => {
       });
   });
 
+  // Server sends back an empty string after client sends data
   it('should handle empty response gracefully (POST)', (done) => {
     axios.post('http://localhost:5503', 'foo bar', { timeout: 30000 })
       .catch((err) => {
@@ -26,7 +29,8 @@ describe('Bad response', () => {
       });
   });
 
-  it('should handle malformed response (immediately upon connection) gracefully', (done) => {
+  // Server sends back a malformed response ("foo bar") immediately upon connection
+  it('should handle malformed response gracefully (GET)', (done) => {
     axios.get('http://localhost:5504', 'foo bar', { timeout: 30000 })
       .catch((err) => {
         expect(err.code).to.equal('HPE_INVALID_CONSTANT');
@@ -34,6 +38,7 @@ describe('Bad response', () => {
       });
   });
 
+  // Server sends back a malformed response ("foo bar") after the client sends data
   it('should handle malformed response gracefully (POST)', (done) => {
     axios.post('http://localhost:5505', 'foo bar', { timeout: 30000 })
       .catch((err) => {
