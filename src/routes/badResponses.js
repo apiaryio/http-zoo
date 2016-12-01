6e58666e1c01558d8175e43798d214337dce9120
Michal Holasek <michal.holasek@apiary.io>
2016-12-01 18:15:52 +0100

    Add 'incomplete' resource to responses

--- src/routes/badResponses.js
+++ src/routes/badResponses.js
@@ -45,6 +45,20 @@ router.get('/failrate', (req, res) => {
   failrateCounter += 1;
 });
 
+router.get('/incomplete', (req, res) => {
+  const data = JSON.stringify(req.headers);
+
+  let response = '';
+  response += 'HTTP/1.1 200 OK\r\n';
+  response += 'Connection: close\r\n';
+  response += `Content-Length: ${data.length}\r\n`;
+  response += 'Content-Type: application/json\r\n';
+  response += 'Date: Wed, 30 Nov 2016 20:19:57 GMT\r\n\r\n';
+  response += data.substring(0, data.length - 7);
+
+  res.socket.end(response);
+});
+
 router.get('/long-running/5', (req, res) => {
   longRunningResponse(req, res, 5000);
 });
