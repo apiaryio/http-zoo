9f507d9ed221e3192ced803af29323a36c0f61c1
XVincentX <vincenzo@apiary.io>
2016-12-16 10:47:35 +0100

    Enable browser tests

---
+++ karma.conf.js
@@ -0,0 +1,38 @@
+
+
+module.exports = function configuration(config) {
+  config.set({
+    basePath: '',
+
+    frameworks: ['browserify', 'mocha'],
+
+    files: [
+      'test/*.js',
+    ],
+
+    exclude: [
+    ],
+
+    preprocessors: {
+      'test/*.js': ['browserify'],
+    },
+
+    reporters: ['progress'],
+
+    port: 9876,
+
+    colors: true,
+
+    logLevel: config.LOG_INFO,
+
+    autoWatch: false,
+
+    browsers: ['Chrome'],
+
+    singleRun: true,
+
+    concurrency: Infinity,
+
+    browserNoActivityTimeout: 30000,
+  });
+};
