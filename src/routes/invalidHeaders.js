37aa5265bc137b48ca5f15de725357216c4ef7c2
XVincentX <vincenzo@apiary.io>
2016-12-16 13:06:44 +0100

    Add cors header for browser testing

--- src/routes/invalidHeaders.js
+++ src/routes/invalidHeaders.js
@@ -22,6 +22,7 @@ router.get('/content-length', (req, res) => {
   response += 'HTTP/1.1 200 OK\r\n';
   response += 'Connection: close\r\n';
   response += 'Content-Length: 3\r\n';
+  response += 'Access-Control-Allow-Origin: *\r\n';
   response += 'Content-Type: text/plain\r\n';
   response += 'Date: Wed, 30 Nov 2016 20:19:57 GMT\r\n\r\n';
   response += 'abcdefghijklmnopqrstuvwxyz';
