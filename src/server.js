c1aa9cb7bbf2c3546c2b1cca73428cf643e22d21
XVincentX <vincenzo@apiary.io>
2016-12-16 10:27:24 +0100

    Do not run brower tests from homemade page

--- src/server.js
+++ src/server.js
@@ -4,9 +4,6 @@ const routes = require('./routes');
 const app = express();
 
 app.set('port', (process.env.PORT || 3000));
-
-app.use('/', express.static(`${__dirname}/public`));
-
 app.use(routes);
 
 app.listen(app.get('port'), () => {
