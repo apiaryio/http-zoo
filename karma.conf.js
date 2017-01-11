d44a3307ec99b179575da32cb9c648f6681684cd
XVincentX <vincenzo@apiary.io>
2017-01-11 16:28:06 +0100

    Test the hamms on firefox as well

--- karma.conf.js
+++ karma.conf.js
@@ -1,38 +1,17 @@
-
-
 module.exports = function configuration(config) {
   config.set({
     basePath: '',
-
     frameworks: ['browserify', 'mocha'],
-
-    files: [
-      'test/*.js',
-    ],
-
-    exclude: [
-    ],
-
-    preprocessors: {
-      'test/*.js': ['browserify'],
-    },
-
+    files: ['test/*.js'],
+    preprocessors: { 'test/*.js': ['browserify'] },
     reporters: ['mocha'],
-
     port: 9876,
-
     colors: true,
-
     logLevel: config.LOG_INFO,
-
     autoWatch: false,
-
-    browsers: ['Chrome'],
-
+    browsers: ['Chrome', 'Firefox'],
     singleRun: true,
-
-    concurrency: Infinity,
-
+    concurrency: 1,
     browserNoActivityTimeout: 35000,
   });
 };
